ae238436aa44ca9809afcc34bcbd3d147575e284
Yaulendil <davarice@protonmail.com>
2020-09-06 21:16:47 -0400

    Improve usage of Features

--- src/util.rs
+++ src/util.rs
@@ -3,12 +3,13 @@ use rand::prelude::*;
 
 /// Percentage chance for a randomly-generated `Message` to contain two
 ///     `Segment`s, separated by a Conjunction.
+#[cfg(any(feature="ds2", feature="ds3", feature="bloodborne"))]
 const COMPOUND_CHANCE: f64 = 0.5;
 
 
 /// Given a `String`, replace the first character with its uppercase equivalent
 ///     in-place. Do not affect any other characters.
-#[allow(dead_code)]
+#[cfg(feature="ds1")]
 pub fn capitalize(s: &mut String) {
     if !s.is_empty() { unsafe { s.as_bytes_mut()[0].make_ascii_uppercase(); } }
 }
@@ -21,7 +22,7 @@ pub fn choose<'a>(slice: &[&'a str], rng: &mut ThreadRng) -> &'a str {
 
 
 /// Randomly choose whether a Message should have multiple components.
-#[allow(dead_code)]
+#[cfg(any(feature="ds2", feature="ds3", feature="bloodborne"))]
 pub fn compound(rng: &mut ThreadRng) -> bool {
     rng.gen_bool(COMPOUND_CHANCE)
 }
